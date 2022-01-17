6d8e3f1bd835a39649cbe3a6088eeabf6b9d6086
dhrubo57 <dhrubo.chowdhury@generalassemb.ly>
2022-01-16 21:28:23 -0500

    second commit

--- Test/ValueAtRiskTest.java
+++ Test/ValueAtRiskTest.java
@@ -12,22 +12,28 @@ public class ValueAtRiskTest {
     List<List<Double>> returnPercentageAll;
     List<Double> weightModified;
     List<Double> demo;
+    List<Double> demo2;
 
     @Before
     public void createPortfolio(){
+        //creating a portfolio with only one stock
         weightModified = new ArrayList<>();
         weightModified.add(1.0);
         weightModified.add(0.0);
 
         returnPercentageAll = new ArrayList<>();
         demo = new ArrayList<>();
+
+        //adding some dummy %p&L values
         demo.add(0.01);
         demo.add(-0.02);
         demo.add(0.03);
-        List<Double> demo2 = new ArrayList<>();
+
+        demo2 = new ArrayList<>();
         demo2.add(0.01);
         demo2.add(-0.02);
         demo2.add(0.03);
+
         returnPercentageAll.add(demo);
         returnPercentageAll.add(demo2);
     }
@@ -36,12 +42,15 @@ public class ValueAtRiskTest {
     @Test
     public void varPortfolioTest(){
 
+        //If the portfolio contains only one stock, then VarSingleTrade and VarPortfolio should be equal
         assertEquals(ValueAtRisk.varSingleTrade(returnPercentageAll.get(0), 95), ValueAtRisk.varPortfolio(returnPercentageAll, weightModified, 95), 0.0001);
 
     }
 
     @Test
     public void varSingleTradeTest(){
+
+        //if the confidence level is 100, value at risk should be max of the %p&L values
         assertEquals(Collections.max(demo), ValueAtRisk.varSingleTrade(demo, 100), 0.000001);
     }
 
--- src/ValueAtRisk.java
+++ src/ValueAtRisk.java
@@ -9,8 +9,10 @@ public class ValueAtRisk {
 
     public static double varSingleTrade(List<Double> returnPercentage, int confidenceInterval){
 
+        //sorting the %p&L values in ascending order
         Collections.sort(returnPercentage);
 
+        //calculating the index to look for in the sorted %p&L values
         int index = returnPercentage.size() * confidenceInterval / 100;
 
         return returnPercentage.get(index - 1);
@@ -19,8 +21,10 @@ public class ValueAtRisk {
 
     public static double varPortfolio(List<List<Double>> returnPercentageAll, List<Double> weights, int confidenceInterval){
 
+        //portfolio to be outputted
         double portfolioVar = 0;
 
+        //looping through the List to calculate varSingleTrade and then applying appropriate weight
         for(int i = 0; i < returnPercentageAll.size(); i++){
 
             portfolioVar += varSingleTrade(returnPercentageAll.get(i), confidenceInterval) * weights.get(i);
@@ -32,10 +36,10 @@ public class ValueAtRisk {
 
     public static void main(String[] args) {
 
-        //System.out.println(varSingleTrade(new int[]{-3, -1, 0, 1, 1, 4, 9}, 95));
-
+        //Netflix historical %p&L values to be inserted in the sampleHistoricalData arrayList
         List<Double> sampleHistoricalData = new ArrayList<>();
 
+        //looping through the txt file and adding the double values to the sampleHistoricalData arrayList
         BufferedReader reader;
         try{
             reader = new BufferedReader(new FileReader("C:\\Users\\dhrub\\IdeaProjects\\Independent Practice\\src\\NFLX_historical_percentage_return.txt"));
@@ -49,8 +53,10 @@ public class ValueAtRisk {
             e.printStackTrace();
         }
 
+        //Creating another arrayList for Apple %p&L values
         List<Double> sampleHistoricalData2 = new ArrayList<>();
 
+        //Repeating the same process for Apple
         BufferedReader reader2;
         try{
             reader2 = new BufferedReader(new FileReader("C:\\Users\\dhrub\\IdeaProjects\\Independent Practice\\src\\AAPL_historical_percentage_return.txt"));
@@ -64,17 +70,21 @@ public class ValueAtRisk {
             e.printStackTrace();
         }
 
+        //creating arrayList for portfolio historical values
         List<List<Double>> portfolioHistoricalData = new ArrayList<>();
         portfolioHistoricalData.add(sampleHistoricalData);
         portfolioHistoricalData.add(sampleHistoricalData2);
 
+        //adding weights
         List<Double> weights = new ArrayList<>();
         weights.add(0.1);
         weights.add(0.9);
 
+        //printing out varSingleTrade for Netflix and Apple at 95% confidence interval
         System.out.println(varSingleTrade(sampleHistoricalData, 95));
         System.out.println(varSingleTrade(sampleHistoricalData2, 95));
 
+        //printing out portfolio var
         System.out.println(varPortfolio(portfolioHistoricalData, weights, 95));
 
     }
